26208d79f4de7e8528cf1e15523ca3e6b9432cff
MFlEGENDANTON <jffgx@mail.ru>
2024-05-17 20:37:07 +0300

    Добавлены возможности удаления и корректного изменения категорий

--- BookShopWeb/Controllers/CategoryController.cs
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -8,7 +8,7 @@ namespace BookShopWeb.Controllers
 	public class CategoryController : Controller
 	{
 
-		
+
 
 		private readonly ApplicationDbContext _Context;
 
@@ -64,25 +64,41 @@ namespace BookShopWeb.Controllers
 			return View(category);
 		}
 
-
 		[HttpPost]
 		public IActionResult Edit(Category obj)
 		{
-			if (obj.Name == obj.DisplayOrder.ToString())
-			{
-				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
-			}
-			if (obj.Name != null && obj.Name.ToLower() == "test")
-			{
-				ModelState.AddModelError("", "Test is an invalid value");
-			}
 			if (ModelState.IsValid)
 			{
-				_Context.Categories.Add(obj);
+				_Context.Categories.Update(obj);
 				_Context.SaveChanges();
 				return RedirectToAction("Index", "Category");
 			}
 			return View();
+		}
+
+
+		public IActionResult Delete(int id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			var category = _Context.Categories.Find(id) ?? throw new ArgumentOutOfRangeException(nameof(id), "Категория с таким ид не существует");
+			//var category1 = _Context.Categories.FirstOrDefault(u => u.Id == id) ?? throw new ArgumentOutOfRangeException(nameof(id), "Категория с таким ид не существует");
+			//var category2 = _Context.Categories.Where(u => u.Id == id).FirstOrDefault() ?? throw new ArgumentOutOfRangeException(nameof(id), "Категория с таким ид не существует");
+			return View(category);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int id)
+		{
+			var obj = _Context.Categories.Find(id) ?? throw new ArgumentOutOfRangeException(nameof(id), "Категория с таким ид не существует");
+
+			_Context.Categories.Remove(obj);
+			_Context.SaveChanges();
+
+			return RedirectToAction("Index", "Category");
 		}
 	}
 }
