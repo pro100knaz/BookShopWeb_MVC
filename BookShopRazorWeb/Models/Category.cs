64752b0668b0c00b84558321e504253774660164
MFlEGENDANTON <jffgx@mail.ru>
2024-05-18 14:04:42 +0300

    ef шаблон для RazorApp

---
+++ BookShopRazorWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookShopRazorWeb.Models
+{
+	public class Category
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[DisplayName("Category Name")]
+		[MaxLength(30, ErrorMessage = "The Field is Required")]
+		public required string Name { get; set; }
+		[DisplayName("Display Order")]
+		[Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+		public int DisplayOrder { get; set; }
+	}
+}
