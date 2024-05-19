c24eda3e1fc83708d9540458471d2231323a7490
MFlEGENDANTON <jffgx@mail.ru>
2024-05-19 20:56:03 +0300

    Добавленно представление описания выбранной книги

--- BookShopWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookShopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -8,31 +8,39 @@ namespace BookShopWeb.Areas.Customer.Controllers
 {
 	[Area("Customer")]
 	public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	{
+		private readonly ILogger<HomeController> _logger;
 		private readonly IUnitOfWork unitOfWork;
 
 		public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-        {
-            _logger = logger;
+		{
+			_logger = logger;
 			this.unitOfWork = unitOfWork;
 		}
 
-        public IActionResult Index()
-        {
-            IEnumerable<Product> products = unitOfWork.Products.GetAll(includeProperties:"Category");
-            return View(products);
-        }
+		public IActionResult Index()
+		{
+			IEnumerable<Product> products = unitOfWork.Products.GetAll(includeProperties: "Category");
+			return View(products);
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Details(int id)
+		{
+			Product product = unitOfWork.Products.Get(u => u.Id == id, includeProperties: "Category");
+			return View(product);
+		}
+
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
