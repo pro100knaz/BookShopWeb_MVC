3e23558c14058c8939e17914c2e62c98a82ce5ad
MFlEGENDANTON <jffgx@mail.ru>
2024-05-19 14:18:52 +0300

    Обьединение Update Create into Upsert

--- BookShopWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookShopWeb/Areas/Admin/Controllers/ProductController.cs
@@ -7,108 +7,98 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookShopWeb.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    public class ProductController : Controller
-    {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly IProductRepository productRepository;
-        private readonly ICategoryRepository categoryRepository;
+	[Area("Admin")]
+	public class ProductController : Controller
+	{
+		private readonly IUnitOfWork unitOfWork;
+		private readonly IProductRepository productRepository;
+		private readonly ICategoryRepository categoryRepository;
 
-        public ProductController(IUnitOfWork unitOfWork)
-        {
-            this.unitOfWork = unitOfWork;
-            productRepository = unitOfWork.Products;
-            categoryRepository = unitOfWork.Category;
+		public ProductController(IUnitOfWork unitOfWork)
+		{
+			this.unitOfWork = unitOfWork;
+			productRepository = unitOfWork.Products;
+			categoryRepository = unitOfWork.Category;
 
 		}
 
-        public IActionResult Index()
-        {
-            var objCategoryList = productRepository.GetAll().ToList();
-            return View(objCategoryList);
-        }
+		public IActionResult Index()
+		{
+			var objCategoryList = productRepository.GetAll().ToList();
+			return View(objCategoryList);
+		}
 
-        public IActionResult Create()
-        {
+		public IActionResult Upsert(int? id)
+		{
 			IEnumerable<SelectListItem> CategoryList = categoryRepository.GetAll().Select(u => new SelectListItem
 			{
 				Text = u.Name,
 				Value = u.Id.ToString()
 			});
+			//ViewBag.CategoryList = CategoryList;
+			//ViewData["CategoryList"] = CategoryList;
+			ProductVM productVM = new()
+			{
+				CategoryList = CategoryList,
+				Product = new Product()
+			};
 
+			if (id == null || id == 0)
+			{
+				//create
+				return View(productVM);
 
+			}
+			else
+			{
+				//update
+				productVM.Product = productRepository.Get(u => u.Id == id);
+				return View(productVM);
+			}
+		}
 
-            //ViewBag.CategoryList = CategoryList;
-            //ViewData["CategoryList"] = CategoryList;
+		[HttpPost]
+		public IActionResult Upsert(ProductVM productVM, IFormFile? file)
+		{
+			if (ModelState.IsValid)
+			{
+				productRepository.Add(productVM.Product);
+				unitOfWork.Save();
+				TempData["success"] = "Product created successfully";
+				return RedirectToAction("Index", "Product");
+			}
+			else
+			{
+				//Иначе может столкнуться с проблемой отсутствия категорий (или же аттрибут ValidateNever) 
+				productVM.CategoryList = categoryRepository.GetAll().Select(u => new SelectListItem
+				{
+					Text = u.Name,
+					Value = u.Id.ToString()
+				});
+				return View(productVM);
+			}
 
+		}
 
-            ProductVM productVM = new()
-            {
-                CategoryList = CategoryList,
-                Product = new Product()
-            };
-			return View(productVM);
-        }
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var product = productRepository.Get(c => c.Id == id);
+			return View(product);
+		}
 
-        [HttpPost]
-        public IActionResult Create(ProductVM obj)
-        {
-            if (ModelState.IsValid)
-            {
-                productRepository.Add(obj.Product);
-                unitOfWork.Save();
-                TempData["success"] = "Product created successfully";
-                return RedirectToAction("Index", "Product");
-            }
-            return View();
-        }
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int id)
+		{
+			var obj = productRepository.Get(c => c.Id == id);
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            var product = productRepository.Get(c => c.Id == id);
-            return View(product);
-        }
-
-
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                productRepository.Update(obj);
-                unitOfWork.Save();
-                TempData["success"] = "Product edited successfully";
-                return RedirectToAction("Index", "Product");
-            }
-            return View();
-        }
-
-
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var product = productRepository.Get(c => c.Id == id);
-            return View(product);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int id)
-        {
-            var obj = productRepository.Get(c => c.Id == id);
-
-            productRepository.Delete(obj);
-            unitOfWork.Save();
-            TempData["success"] = "Product deleted successfully";
-            return RedirectToAction("Index", "Product");
-        }
-    }
+			productRepository.Delete(obj);
+			unitOfWork.Save();
+			TempData["success"] = "Product deleted successfully";
+			return RedirectToAction("Index", "Product");
+		}
+	}
 }
